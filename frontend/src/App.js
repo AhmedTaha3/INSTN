7f7a2e14dcef3799f4f814f5af8873281f30a413
AhmedTaha3 <bekahmad36@gmail.com>
2024-07-16 15:03:57 +0300

    src

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import UserForm from './components/UserForm';
+
+function App() {
+    return (
+        <div className="App">
+            <UserForm />
+        </div>
+    );
+}
+
+export default App;
